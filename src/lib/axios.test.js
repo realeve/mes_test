2f9b37a5c6e8d730ac8cec904ff936392bd0de74
realeve@qq.com <realeve@qq.com>
2018-12-28 23:44:38 +0800

    update axios unit test demo

--- src/lib/axios.test.js
+++ src/lib/axios.test.js
@@ -9,3 +9,13 @@ test('axios 传入数组参数', async () => {
   });
   expect(res.header).toHaveLength(2);
 });
+
+test('axios 报错', () => {
+  // expect.assertions(1);
+  return expect(
+    axios({ url: 'http://api.cbpc.ltd/444' })
+  ).rejects.toMatchObject({
+    data: { msg: '不存在的路由:444', status: '500', url: '444' },
+    description: '服务器发生错误，请检查服务器。不存在的路由:444'
+  });
+});
